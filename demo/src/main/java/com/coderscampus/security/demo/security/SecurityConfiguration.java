beba5d6a00faef2994129623142c648f39e3a02d
deividas strole <deiwis@yahoo.com>
2024-06-21 22:30:56 -0700

    going forward

--- demo/src/main/java/com/coderscampus/security/demo/security/SecurityConfiguration.java
+++ demo/src/main/java/com/coderscampus/security/demo/security/SecurityConfiguration.java
@@ -1,15 +1,31 @@
 package com.coderscampus.security.demo.security;
 
+import com.coderscampus.security.demo.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService(passwordEncoder());
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,8 +35,17 @@ public class SecurityConfiguration {
                     .anyRequest()
                     .permitAll();
         })
+        .authenticationProvider(authenticationProvider())
         .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        return daoAuthenticationProvider;
     }
 
 }
---
+++ demo/src/main/java/com/coderscampus/security/demo/service/UserService.java
@@ -0,0 +1,25 @@
+package com.coderscampus.security.demo.service;
+
+import com.coderscampus.security.demo.domain.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserService implements UserDetailsService {
+
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(PasswordEncoder passwordEncoder) {
+        super();
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = new User(username, passwordEncoder.encode("abc123"));
+
+        return user;
+    }
+}
